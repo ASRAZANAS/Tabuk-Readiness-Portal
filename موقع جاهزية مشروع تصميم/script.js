643458842ed2c98c3a58bf120fbca4137cdeaa63
Razan AL-hmdi <108956655+ASRAZANAS@users.noreply.github.com>
2026-05-05 23:03:07 +0300

    Add files via upload

---
+++ موقع جاهزية مشروع تصميم/script.js
@@ -0,0 +1,27 @@
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    background-image: url('tabuk-university.jpg'); 
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    line-height: 1.8;
+    color: #2c3e50;
+}
+
+header {
+    background-color: #e8f5e9; 
+    color: #004733;
+    padding: 30px 0;
+    text-align: center;
+    border-bottom: 3px solid #81c784;
+}
+
+.university-logo {
+    width: 90px;
+    height: auto;
+    margin-bottom: 10px;
+    display: block;
+    margin
